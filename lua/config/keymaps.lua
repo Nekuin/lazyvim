17ce2e90d2b41c6b9f2da1478c116cc0561c0191
simo <nekuin@gmail.com>
2025-05-20 15:19:27 +0300

    couple keybinds and disable smooth scrolling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "C-d + zz" })
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "C-u + zz" })
+vim.keymap.set("n", "<leader>r", ":%s/<C-r><C-w>//g<Left><Left>", { desc = "Search and replace" })
