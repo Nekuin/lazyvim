f5bbb7937045e1143691acb5b82a55e535830b4b
simo <nekuin@gmail.com>
2025-06-05 13:53:32 +0300

    rspec neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,15 +4,14 @@ return {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
-    "olimorris/neotest-rspec",
-    "zidhuss/neotest-minitest",
+    "nekuin/neotest-rspec",
   },
   config = function()
     require("neotest").setup({
       adapters = {
-        require("neotest-minitest")({
-          test_cmd = function()
-            local command = vim.tbl_flatten({
+        require("neotest-rspec")({
+          rspec_cmd = function(position_type)
+            return vim.tbl_flatten({
               "docker",
               "compose",
               "exec",
@@ -20,23 +19,11 @@ return {
               "bin/rails",
               "test",
             })
-
-            vim.print("Neotest Minitest test_cmd:", command)
-
-            return command
           end,
-
           transform_spec_path = function(path)
-            local prefix = require("neotest-minitest").root(path)
-            local transformed_path = string.sub(path, string.len(prefix) + 2, -1)
-            local line = vim.fn.line(".")
-
-            vim.print("Neotest Minitest path:", { original = path, transformed_path })
-
-            return transformed_path .. ":" .. line
+            local prefix = require("neotest-rspec").root(path)
+            return string.sub(path, string.len(prefix) + 2, -1)
           end,
-
-          results_path = "tmp/minitest.output",
         }),
       },
     })
